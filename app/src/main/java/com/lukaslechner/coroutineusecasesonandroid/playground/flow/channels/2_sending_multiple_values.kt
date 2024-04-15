d2a1647a0ab859698f3d4bebb6e87e7f2c4532aa
kotomoto <jkotor@gmail.com>
2024-04-15 22:34:52 +0200

    Add channels example

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/channels/2_sending_multiple_values.kt
@@ -0,0 +1,28 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.channels
+
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+suspend fun main(): Unit = coroutineScope {
+
+    val channel = produce<Int> {
+        println("Sending 10")
+        send(10)
+        println("Sending 20")
+        send(20)
+    }
+
+    launch {
+        channel.consumeEach { receivedValue ->
+            println("Consumer 1: $receivedValue")
+        }
+    }
+
+    launch {
+        channel.consumeEach { receivedValue ->
+            println("Consumer 2: $receivedValue")
+        }
+    }
+}
