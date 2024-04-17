2e1c55c35ed266fb6c0601d52e7a4581e859906f
kotomoto <jkotor@gmail.com>
2024-04-17 22:55:43 +0200

    Add buffer in shared flow sample

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/concurrency/9_buffer_in_sharedflow.kt
@@ -0,0 +1,36 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.concurrency
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
+
+suspend fun main(): Unit = coroutineScope {
+
+    val flow = MutableSharedFlow<Int>(extraBufferCapacity = 10)
+
+    launch {
+        flow.collect {
+            println("Collector 1 processes $it")
+        }
+    }
+
+    launch {
+        flow.collect {
+            println("Collector 2 processes $it")
+            delay(100)
+        }
+    }
+
+
+    launch {
+        val timeToEmit = measureTimeMillis {
+            repeat(5) {
+                flow.emit(it)
+                delay(10)
+            }
+        }
+        println("Time to emit all values: $timeToEmit ms")
+    }
+}
