8c3414cdef613a5ef8ec813ead92272d0751857c
kotomoto <jkotor@gmail.com>
2024-04-29 14:17:50 +0200

    Add sample for playing with virtual time in unit test

---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/playground/1_virtual_time.kt
@@ -0,0 +1,49 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class SystemUnderTest {
+
+    suspend fun functionWithDelay(): Int {
+        delay(1000)
+        return 42
+    }
+}
+
+fun CoroutineScope.functionThatStartsNewCoroutine() {
+    launch {
+        delay(1000)
+        println("Coroutine completed")
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TestClass {
+    @Test
+    fun `functionWithDelay() should return 42`() = runTest {
+        val realTimeStart = System.currentTimeMillis()
+        val virtualTimeStart = currentTime
+
+        functionThatStartsNewCoroutine()
+        advanceTimeBy(1001)
+
+//        val sut = SystemUnderTest()
+//
+//        val actual = sut.functionWithDelay()
+//
+//        Assert.assertEquals(42, actual)
+
+        val realTimeDuration = System.currentTimeMillis() - realTimeStart
+        val virtualTimeDuration = currentTime - virtualTimeStart
+
+        println("Test took $realTimeDuration real ms")
+        println("Test took $virtualTimeDuration virtual ms")
+    }
+}
